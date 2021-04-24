c992c0c70bc105fba41a30950993189f60374ee5
josh mitchell <jshmtchll@gmail.com>
2021-04-24 10:46:38 -0500

    htmlroutes set

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const router = require('express').Router();
+const htmlRoutes = require('./routes/htmlRoutes');
+const apiRoutes = require('./routes/apiRoutes');
+
+// initial express app //
+const PORT = process.env.PORT || 3030;
+const app = express();
+
+
+//parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+//parse incoming JSON data
+app.use(express.json());
+app.use(express.static("public"));
+
+
+//requires routes //
+app.use('/', htmlRoutes);
+app.use('/api', apiRoutes);
+
+
+
+
+
+
+
+
+
+// listener //
+app.listen(PORT, () => {
+    console.log(`App is running on port: ${PORT}` +  " ¯\\_(ツ)_/¯");
+});
